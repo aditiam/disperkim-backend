6cdbb997723e57b70344558b10a849757c31bd37
aditiam23 <aditiamuchlis23@gmail.com>
2025-01-27 04:14:16 +0700

    hua

--- config/database.js
+++ config/database.js
@@ -1,3 +1,5 @@
+// require('dotenv').config();
+
 module.exports = ({ env }) => ({
   connection: {
     client: "postgres",
@@ -5,8 +7,8 @@ module.exports = ({ env }) => ({
       host: env("DATABASE_HOST", "127.0.0.1"),
       port: env.int("DATABASE_PORT", 5432),
       database: env("DATABASE_NAME", "disperkimweb"),
-      user: env("DATABASE_USERNAME", "asyaky"),
-      password: env("DATABASE_PASSWORD", `Assassin'sCreed1`),
+      user: env("DATABASE_USERNAME", ""),
+      password: env("DATABASE_PASSWORD", ``),
       ssl: env.bool("DATABASE_SSL", false),
     },
   },
