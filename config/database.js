8baa1752915eff018776cd757b00fd917871f792
aditiam23 <aditiamuchlis23@gmail.com>
2025-01-27 04:53:59 +0700

    hua

--- config/database.js
+++ config/database.js
@@ -8,7 +8,7 @@ module.exports = ({ env }) => ({
       port: env.int("DATABASE_PORT", 5432),
       database: env("DATABASE_NAME", "disperkimweb"),
       user: env("DATABASE_USERNAME", ""),
-      password: env("DATABASE_PASSWORD", ``),
+      password: env("DATABASE_PASSWORD", ""),
       ssl: env.bool("DATABASE_SSL", false),
     },
   },
